abd74387e6f472c9284eab9cdf4014747ee2e69c
Matteo Amagliani <matteo.amagliani@studenti.unipd.it>
2025-12-09 18:28:21 +0100

    Setup API Gold route

--- src/etl_studio/api/main.py
+++ src/etl_studio/api/main.py
@@ -3,10 +3,12 @@
 from fastapi import FastAPI
 
 from etl_studio.api.routes.bronze import router_bronze
+from etl_studio.api.routes.gold import router_gold
 
 app = FastAPI(title="ETL Studio API", version="0.1.0")
 
 app.include_router(router_bronze)
+app.include_router(router_gold)
 
 
 @app.get("/")
