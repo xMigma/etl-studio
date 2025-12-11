bb372256c1de3f7f4198c4ec818087d73373f1eb
Codeltierra <clara.dorado@alu.uclm.es>
2025-12-11 11:43:32 +0100

    add get_preview_from_bronze function in silver layer

--- src/etl_studio/config.py
+++ src/etl_studio/config.py
@@ -4,4 +4,4 @@ from __future__ import annotations
 
 import os
 
-API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8000")
+API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:80")
--- src/etl_studio/postgres/silver.py
+++ src/etl_studio/postgres/silver.py
@@ -8,6 +8,15 @@ from sqlalchemy import text
 from etl_studio.postgres.postgres import get_engine, clean_table_name
 
 
+def get_preview_from_bronze(table_name: str, limit: int = 5) -> pd.DataFrame:
+    """Get a preview of a specific table from the bronze schema."""
+    cleaned_name = clean_table_name(table_name)
+    engine = get_engine()
+    
+    query = text("SELECT * FROM bronze.:table_name LIMIT :limit")
+    return pd.read_sql(query, engine, params={"table_name": cleaned_name, "limit": limit})
+
+
 def get_table_from_bronze(table_name: str) -> pd.DataFrame:
     """Get content of a specific table from the bronze schema."""
     cleaned_name = clean_table_name(table_name)
